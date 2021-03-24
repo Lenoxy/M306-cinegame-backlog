832b5370302fb302ca89ba2c1f315952cba9d8c4
the-philip-baumann <philip.baumann@hispeed.ch>
2021-03-24 12:42:18 +0100

    spring backend setup

---
+++ backend-cinegame-backlog/src/main/java/ch/css/lernende/backendcinegamebacklog/BackendCinegameBacklogApplication.java
@@ -0,0 +1,13 @@
+package ch.css.lernende.backendcinegamebacklog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCinegameBacklogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCinegameBacklogApplication.class, args);
+	}
+
+}
---
+++ backend-cinegame-backlog/src/test/java/ch/css/lernende/backendcinegamebacklog/BackendCinegameBacklogApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.css.lernende.backendcinegamebacklog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCinegameBacklogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
