ecdffa88f203ccf438d2b314c12a125f264a2324
Raphael Härtel <raphael.haertel@bluewin,ch>
2021-05-12 13:56:14 +0200

    Implement register layout

--- frontend-cinegame-backlog/src/app/register/register.component.ts
+++ frontend-cinegame-backlog/src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -6,8 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  public username = '';
+  public email = '';
+  public password = '';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
