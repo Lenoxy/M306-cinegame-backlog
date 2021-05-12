417b92f2e02e6d74583fd80ceb53f787fe43504b
the-philip-baumann <philip.baumann@hispeed.ch>
2021-05-12 12:45:50 +0200

    Add Lombok

--- backend-cinegame-backlog/src/main/java/ch/css/lernende/backendcinegamebacklog/dto/LoginInDto.java
+++ backend-cinegame-backlog/src/main/java/ch/css/lernende/backendcinegamebacklog/dto/LoginInDto.java
@@ -1,27 +1,17 @@
 package ch.css.lernende.backendcinegamebacklog.dto;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class LoginInDto{
     private String username;
     private String password;
 
     public LoginInDto(String username, String password){
         this.username = username;
-        this.password = password;
-    }
-
-    public String getUsername(){
-        return username;
-    }
-
-    public void setUsername(String username){
-        this.username = username;
-    }
-
-    public String getPassword(){
-        return password;
-    }
-
-    public void setPassword(String password){
         this.password = password;
     }
 }
