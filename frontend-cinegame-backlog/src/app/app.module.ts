30abe2a34e11cf546ed328e919a7824febf8852a
the-philip-baumann <philip.baumann@hispeed.ch>
2021-05-26 14:18:59 +0200

    Implement sidebar, took so long because these weasels forced me to make a sidebar

--- frontend-cinegame-backlog/src/app/app.module.ts
+++ frontend-cinegame-backlog/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { OverviewComponent } from './overview/overview.component';
 import { EditItemComponent } from './edit-item/edit-item.component';
 import { NewItemComponent } from './new-item/new-item.component';
 import { ProfileComponent } from './profile/profile.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   imports: [
@@ -31,7 +32,8 @@ import { ProfileComponent } from './profile/profile.component';
     OverviewComponent,
     EditItemComponent,
     NewItemComponent,
-    ProfileComponent
+    ProfileComponent,
+    SidebarComponent
   ],
   bootstrap: [ AppComponent ]
 })
---
+++ frontend-cinegame-backlog/src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  async navigateToOverviewWithParam(film: string): Promise<void> {
+    await this.router.navigate(['/overview'], )
+  }
+
+  async logout(): Promise<void> {
+    await this.router.navigate(['/login'])
+  }
+}
