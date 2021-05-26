04eda49cdd32198d4961af3c11d6930e8ee4442a
the-philip-baumann <philip.baumann@hispeed.ch>
2021-05-26 13:18:32 +0200

    Implement change password

--- frontend-cinegame-backlog/src/app/reset-password/reset-password.component.ts
+++ frontend-cinegame-backlog/src/app/reset-password/reset-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-reset-password',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reset-password.component.css']
 })
 export class ResetPasswordComponent implements OnInit {
+  password: string;
+  passwordConfirmation: string;
 
-  constructor() { }
+  valid: boolean;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  async submit(): Promise<void> {
+    if (!this.passwordAreEqual() || !this.passwordAreGreaterThanMinLength()) {
+      this.valid = true
+    } else {
+      await this.router.navigate(["/"])
+    }
+  }
+
+
+  passwordAreEqual(): boolean {
+    return this.passwordConfirmation == this.password
+  }
+
+  private passwordAreGreaterThanMinLength(): boolean {
+    return this.password && this.password.length > 5;
+
+  }
 }
