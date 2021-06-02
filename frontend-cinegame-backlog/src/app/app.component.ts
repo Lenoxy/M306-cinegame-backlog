d3871951a98714ca1c4806e870326831249f3e5e
daniel <danypolgar@gmail.com>
2021-06-02 13:14:20 +0200

    login fixed

--- frontend-cinegame-backlog/src/app/app.component.ts
+++ frontend-cinegame-backlog/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {CookieService} from "ngx-cookie-service";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private cookieService: CookieService) {
+  }
   title = 'Cinegame Backlog';
+
+  authenticated(): boolean {
+    return this.cookieService.get('auth') === 'true';
+  }
 }
